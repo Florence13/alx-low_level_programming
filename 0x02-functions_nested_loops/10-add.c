bc1994f86297943616f6f857c8b69593bc0a9e46
Florence13 <chukwuflorence13>
2022-09-14 15:01:49 -0700

    commit for 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* add - check description
+* @a : first int parameter
+* @b : second int parameter
+* Description: adds two numbers passed to it
+* Return: the sum of the arguments
+*/
+int add(int a, int b)
+{
+	return (a + b);
+
+}
