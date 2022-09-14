d7f7c88d7cd55680697db4dd2d5108f236b1f008
Florence13 <chukwuflorence13>
2022-09-14 14:47:15 -0700

    final commit or 9-times_table.c project

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+* times_table - check description
+* Description: it prints 0-9 times table
+* Return: Nothing.
+*/
+void times_table(void)
+{
+	int j;
+	int i;
+	int n = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			n = i * j;
+			if (n > 9)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+			else if (n <= 9 && j != 0)
+			{
+				_putchar(' ');
+				_putchar(n + '0');
+			}
+			else if (n <= 9 && j == 0)
+				_putchar(n + '0');
+			if (j == 9)
+				continue;
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+
+}
