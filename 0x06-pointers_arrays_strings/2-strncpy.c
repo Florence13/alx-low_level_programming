d6d2e91277747a49864405559d1dfc36812f3ea3
Florence13 <chukwuflorence13>
2022-09-23 16:40:58 -0700

    commit for 2-strncpy.c final

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* *_strncpy - check description
+* @dest : string output parameter
+* @src : string input parameter
+* @n : int input parameter
+* Description: copies a string into another string
+* Return: pointer to the resulting string
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int srcL = 0;
+	char *beg = src;
+	char *val = dest;
+	int i;
+
+
+	while (*src)
+	{
+		srcL++;
+		src++;
+	}
+	srcL++;
+
+	if (n > srcL)
+		n = srcL;
+
+	src = beg;
+
+	for (i = 0; i < n; i++)
+		*dest++ = *src++;
+
+	return (val);
+
+}
